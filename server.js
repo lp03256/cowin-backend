f49aed94c8493badaea6cb4e8a802cca15f6ab53
lpatil <lpatil@digite.com>
2022-11-10 15:56:11 +0530

    DEF3 Updating server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const nodemailer = require('nodemailer');
 const moment = require('moment');
 const fetch = require("node-fetch");
 
-
 //App Config
 const app = express();
 const port = process.env.PORT || 8001;
